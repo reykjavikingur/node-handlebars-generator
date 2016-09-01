eb23fa12a200afc044ff6b80d967401df99bcef8
Andrew Strader <astrader@resourceammirati.com>
2016-09-01 12:20:24 -0400

    add implementation and a few tests

---
+++ test/index.spec.js
@@ -0,0 +1,54 @@
+var should = require('should');
+var HandlebarsGenerator = require('../');
+var Handlebars = require('handlebars');
+var FileTree = require('web-template-file-tree');
+
+describe('HandlebarsGenerator', function () {
+
+	it('should be truthy', function () {
+		should(HandlebarsGenerator).be.ok();
+	});
+
+	it('should initially have 0 pages', function () {
+		should(HandlebarsGenerator.pages).eql([]);
+	});
+
+	describe('.registerPage', function () {
+		beforeEach(function () {
+			HandlebarsGenerator.registerPage('index', 'home', {title: 'Welcome'});
+		});
+		it('should add 1 page', function () {
+			should(HandlebarsGenerator.pages.length).equal(1);
+		});
+	});
+
+	describe('.compilePage', function () {
+
+		var template, data;
+
+		beforeEach(function () {
+			// create stubs
+			template = 'You are at the {{title}} site.';
+			data = {title: 'Welcome'};
+
+			HandlebarsGenerator.srcTree = new FileTree('src');
+			HandlebarsGenerator.srcTree.cache = {
+				'home': template
+			};
+			HandlebarsGenerator.distTree = new FileTree('dist');
+			HandlebarsGenerator.distTree.cache = {};
+		});
+
+		beforeEach(function(){
+			HandlebarsGenerator.compilePage('index', 'home', data);
+		});
+
+		it('should render template', function () {
+			var actual = HandlebarsGenerator.distTree.cache['index'];
+			var expected = Handlebars.compile(template)(data);
+			should(actual).equal(expected);
+		});
+		
+	});
+
+});
