5f00f3de417fb0289c9e8d0ecbb77b4f31272d09
Andrew Strader <astrader@resource.com>
2016-10-09 19:49:31 -0400

    create e2e test script

---
+++ e2e/index.js
@@ -0,0 +1,25 @@
+// test every e2e case
+
+var cp = require('child_process');
+var execFileSync = cp.execFileSync;
+
+runTest('basic');
+
+function runTest(path) {
+	var dir = __dirname + '/' + path;
+	execFileSync('rm', ['-rf', dir + '/dist']);
+	execFileSync('node', [dir + '/index.js']);
+	var diffError;
+	try {
+		execFileSync('/usr/bin/diff', ['-r', dir + '/expected', dir + '/dist'], {encoding:'utf8'});
+	}
+	catch (e) {
+		diffError = e;
+	}
+	if (diffError) {
+		console.error('differences detected in "' + path + '" case');
+	}
+	else {
+		console.log('case "' + path + '" passed');
+	}
+}
